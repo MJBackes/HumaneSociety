931c37ea3c260ee35a96628fca4225401b520d14
MJBackes <mjohnbackes@gmail.com>
2019-11-19 11:31:07 -0600

    Tested the Search by Multiple traits method.

--- HumaneSociety/Program.cs
+++ HumaneSociety/Program.cs
@@ -11,6 +11,8 @@ namespace HumaneSociety
         static void Main(string[] args)
         {
             //PointOfEntry.Run();
+            Animal animal = new Animal { CategoryId = 1, Name = "Dog2", Age = 3, Demeanor = "Dog - y", KidFriendly = true, PetFriendly = true, Weight = 29, };
+            Query.AddAnimal(animal);
             Dictionary<int, string> input = new Dictionary<int, string>();
             input.Add(1, "1");
             UserInterface.DisplayAnimals(Query.SearchForAnimalsByMultipleTraits(input).ToList());
